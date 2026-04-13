77589f19e0fa10422cc925e9d03e3195115d6ac0
Kanishka Solanki <kanishkasolanki456s@gmail.com>
2026-04-14 04:04:52 +0530

    add necessary function definitions

--- LinearRegressor.h
+++ LinearRegressor.h
@@ -5,4 +5,15 @@
 #ifndef LINEARREGRESSOR_H
 #define LINEARREGRESSOR_H
 
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class LinearRegression {
+public:
+    LinearRegression(vector<vector<double>> input, vector<double> actual_val);
+    double MSE(vector<double> pred, vector<double> actual);
+    pair<vector<double>,double> gradient_descent(double learn_rate, int epoch, vector<vector<double>> input, vector<double> act_v);
+};
+
 #endif //LINEARREGRESSOR_H
--- example.cpp
+++ example.cpp
@@ -2,3 +2,8 @@
 // Created by kanishka on 14/4/26.
 //
 
+#include "LinearRegressor.h"
+
+int main() {
+    return 0;
+}
