d9097d1ae5ab5c158a9d6122be023b25398f98a4
Kanishka Solanki <kanishkasolanki456s@gmail.com>
2026-04-14 04:14:34 +0530

    add MSE function

--- LinearRegressor.cpp
+++ LinearRegressor.cpp
@@ -1,3 +1,16 @@
 //
 // Created by kanishka on 14/4/26.
 //
+
+#include "LinearRegressor.h"
+
+double LinearRegression::MSE(vector<double> pred, vector<double> actual) {
+    int num = pred.size();
+    double error;
+    for (int i=0;i<num;i++) {
+        int err = actual[i] - pred[i];
+        err *= err;
+        error +=err;
+    }
+    return error;
+}
