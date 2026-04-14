d04e09c920794aa4f25db1a83b01af6cb1434516
Kanishka Solanki <kanishkasolanki456s@gmail.com>
2026-04-14 22:07:55 +0530

    fix gradient descent

--- LinearRegressor.cpp
+++ LinearRegressor.cpp
@@ -4,12 +4,12 @@
 
 #include "LinearRegressor.h"
 
-double LinearRegression::MSE(double pred, double actual) {
+double LinearRegression::find_error(double pred, double actual) {
     double err = actual - pred;
-    return err * err;
+    return err;
 }
 
-LinearRegression::LinearRegression(vector<vector<double> > input, vector<double> actual_val) {
+LinearRegression::LinearRegression(vector<vector<double>>& input, vector<double>& actual_val) {
     double learn_rate = 0.0001;   //we are keeping learning rate so small to account for differentiate values and to prevent overshooting
     int epochs = 1000;   //no. of times we iterate over a data set
     int n = input[0].size();
@@ -18,6 +18,7 @@ LinearRegression::LinearRegression(vector<vector<double> > input, vector<double>
     pair<vector<double>, double> result;
     result = gradient_descent(learn_rate, epochs, input, actual_val, w, bias);
     get_features(n);
+    predicted_val = 0;
     for (int i=0;i<n;i++) {
         predicted_val += result.first[i] * input_features[i];
     }
@@ -33,7 +34,7 @@ void LinearRegression::get_features(int n) {
     }
 }
 
-pair<vector<double>, double> LinearRegression::gradient_descent(double learn_rate, int epochs, vector<vector<double>> input, vector<double> act_v, vector<double> w, double b) {
+pair<vector<double>, double> LinearRegression::gradient_descent(double learn_rate, int epochs, vector<vector<double>>& input, vector<double>& act_v, vector<double>& w, double b) {
     int row = input.size();
     int feat = input[0].size();
     for (int i=0;i<epochs;i++) {
@@ -43,14 +44,15 @@ pair<vector<double>, double> LinearRegression::gradient_descent(double learn_rat
                 predic += w[k]* input[j][k];
             }
             predic += b;
-            double error = MSE(predic, act_v[j]);
-            //differentiating the error
-            for (int k=0; k<n; k++){
-                w[k] += -(1.0/row)*(x[j][k])*error;
+            double error = find_error(predic, act_v[j]);
+            //differentiating the error and multiplying by learning rate
+            for (int k=0; k<feat; k++){
+                w[k] -= learn_rate * input[j][k] * error;
             }
-            b += -(1.0/row)*error;
+            b -= learn_rate * error;
         }
     }
+    return {w,b};
 }
 
 
--- LinearRegressor.h
+++ LinearRegressor.h
@@ -12,9 +12,9 @@ using namespace std;
 class LinearRegression {
     vector<double> input_features;  //these are the features we are predicting for
 public:
-    LinearRegression(vector<vector<double>> input, vector<double> actual_val);
-    double MSE(double pred, double actual);
-    pair<vector<double>,double> gradient_descent(double learn_rate, int epochs, vector<vector<double>> input, vector<double> act_v, vector<double> w, double b);
+    LinearRegression(vector<vector<double>>& input, vector<double>& actual_val);
+    double find_error(double pred, double actual);
+    pair<vector<double>,double> gradient_descent(double learn_rate, int epochs, vector<vector<double>>& input, vector<double>& act_v, vector<double>& w, double b);
     double predicted_val = 0;
     void get_features(int n);
 };
